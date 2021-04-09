d7ca2d0aaa21a18c1f410699a44939b34d2ff291
Amen <jamen.jamen30@gmail.com>
2021-04-09 17:54:32 +0300

    README and Black/Size feature

---
+++ etch-a-sketch/README.md
@@ -0,0 +1,22 @@
+# Etch a Sketch
+
+![The game page](gif/home.gif)
+
+> Drawing 
+
+---
+
+## Description
+
+ The Etch-a-Sketch is a web game where users create pixel art, they have two options, black boxes, or colorful boxes, they also can change the boxes numbers.
+
+## How To Use
+ You just need to **hover** over the boxes! By hovering over the boxes their color will change.
+
+ - ### Colors
+ To change the drawing color to black, just click on the `Black` button, to make it return to **default** click on the `Colorful` button. The `Reset` button will clear all boxes.
+
+ - ### Size
+ To change the size, move the slider to the size you want, **16 x 16** for example, and click on `Change Size`, the size will change and the boxes will get transparent till you hover over them another time.
+    
+---
--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,36 +1,75 @@
 const container = document.getElementById('container');
+const blackButton = document.getElementById('black');
+const colorfulButton = document.getElementById('colorful');
+let newSize = document.getElementById('newsize');
+let displaySize = document.getElementById('sizelabel');
+ 
+// it gives a random color
+function randomColor(e){
+    e.target.style.background = '#' + Math.floor(Math.random()*16777215).toString(16);
+}
 
+// it gives black color
+function blackColor(e){
+    e.target.style.background = 'black';
+}
+// it makes the boxes
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
       let cell = document.createElement("div");
-      cell.addEventListener('mouseover', function(){
-          this.style.background = '#' + Math.floor(Math.random()*16777215).toString(16);
-         });
+      cell.addEventListener('mouseover', randomColor);
       container.appendChild(cell).className = "grid-item";
     };
   };
+ 
+// making boxes  
 makeRows (10 ,10)
 
+// identifying the size that user wants
 $('.size').click(function(){
-    var size = prompt('pick a number for boxes size');
-    size = parseInt(size);
-    if (size > 64 || size < 1  || Number.isNaN(size)){
-        alert('please insert a number in range 1-64');
-    } else {
-        removeBoxes(container);
-        makeRows(size, size);
-    }
+    var size = newSize.value;
+    removeBoxes(container);
+    makeRows(size, size);
 })  
 
+// cleaning everything when user click on reset button and making new boxes
 $('.reset').click(function(){
-    removeBoxes(container);
-    makeRows(10, 10);
+    var boxes = document.querySelectorAll('.grid-item')
+    boxes.forEach((box) => {
+        box.style.background = '';
+    })
 })
+
+// deleting every box
 function removeBoxes (parent) {
     while (parent.firstChild){
         parent.removeChild(parent.firstChild)
     }
 }
 
+// it changes the number beside the range to same range value
+newSize.value = 10;
+displaySize.textContent = `${newSize.value} x ${newSize.value}`;
+newSize.addEventListener('mousemove', function() {
+  displaySize.textContent = `${newSize.value} x ${newSize.value}`;
+})
+
+// after clicking on button the hovering will makes boxes black
+blackButton.addEventListener('click', function(){
+    let boxes = document.querySelectorAll('.grid-item');
+    boxes.forEach((box) => {
+        box.removeEventListener('mouseover', randomColor)
+        box.addEventListener('mouseover', blackColor)
+    })
+})
+
+// after clicking on button the hovering will makes boxes colorful(default)
+colorfulButton.addEventListener('click', function(){
+    let boxes = document.querySelectorAll('.grid-item');
+    boxes.forEach((box) => {
+        box.removeEventListener('mouseover', blackColor)
+        box.addEventListener('mouseover', randomColor)
+    })
+})
